15fc373637612fa86cbb41307a173eaaf8f746d7
Jeremiah Church <jeremiahchurch@gmail.com>
2025-02-26 07:05:59 +0000

    Testing batch contacts

--- src/helpers.js
+++ src/helpers.js
@@ -14,3 +14,14 @@ export const sendWebhook = async (data) => {
     .then((text) => console.log('Message sent to webhook:', data.key, text))
     .catch((err) => console.error('Error sending message to webhook:', err))
 }
+
+export const sendContactsWebhook = async (data) => {
+  fetch(WEBHOOK_URL, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ job_id: JOB_ID, bulk_contacts: true, data })
+  })
+    .then((res) => res.text())
+    .then((text) => console.log('Contact sent to webhook:', data.key, text))
+    .catch((err) => console.error('Error sending contact to webhook:', err))
+}
