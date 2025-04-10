2cc3f3de7873b3f411d0443fc44b18fae678e257
Jeremiah Church <jeremiahchurch@gmail.com>
2025-04-10 14:26:22 +0500

    [SC-9101] Adding alha whatsapp

--- src/whatsappClient.js
+++ src/whatsappClient.js
@@ -73,10 +73,16 @@ export function initializeWhatsAppClient () {
     if (message.isStatus) return
 
     let mediaData = null;
-    if (message.from?.includes("@c.us")) {
+    const chat_id = message?.id?.remote
+    if (chat_id?.includes("@g.us")) {
+      const chat = await client.getChatById(chat_id)
+      message.participants = chat?.participants
+      message.isGroup = true
+    } else if (chat_id?.includes("@c.us")) {
       const reciever_contact = await client.getContactById(message.to);
       message.reciever_name = reciever_contact.name || reciever_contact.pushname
       message.reciever_phone = reciever_contact.number
+      message.isGroup = false
     }
     if (message.hasMedia && message.type == 'image') {
 
